7607c7c1793f5f5cf76799929f8836fcccd82164
mayaratlt23 <mayaratalyta25@gmail.com>
2025-08-14 19:54:36 -0300

    Exercicios

---
+++ script.js
@@ -0,0 +1,13 @@
+idade = 28
+console.log('Minha idade atual:', idade)
+
+idade = 29
+console.log('Minha idade depois do aniversario:', idade)
+
+dataAniversario = '17.02.1997'
+console.log('Minha de aniversário é:', dataAniversario)
+
+
+
+
+
