a8f1e47a91d22e4fb2c14ef2ad83c17cda3a231e
mayaratlt23 <mayaratalyta25@gmail.com>
2025-08-26 19:57:30 -0300

    Exercicios

---
+++ operadoresMatematicos.js
@@ -0,0 +1,37 @@
+const numeroUm = 4
+const numeroDois = 8
+
+const subtracao = numeroUm - numeroDois
+console.log('Subtração: ', subtracao)
+
+const soma = numeroUm + numeroDois
+console.log('Soma: ', soma)
+
+const divisao = numeroUm / numeroDois
+console.log('Divisão: ', divisao)
+
+const multiplicacao = numeroUm * numeroDois
+console.log('Multiplicação: ', multiplicacao)
+
+const contaComplexa = (numeroUm + numeroDois) * numeroUm
+console.log('Conta complexa: ', contaComplexa)
+
+const resto = 12 % 2
+console.log('Resto da divisão: ', resto)
+
+const potencia = 2 ** 3
+console.log('Potência: ', potencia)
+
+let contador = 5
+
+// contador = contador + 1
+contador++
+console.log('Incremento: ', contador)
+
+contador = contador - 1
+console.log('Decremento: ', contador)
+
+let numeroTres = 2
+
+numeroTres *= 3
+console.log(numeroTres)
